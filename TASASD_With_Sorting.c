72707fa07b80e818ec290a1b7faeb1d5e2530752
Skalter <130365252+Skalter19@users.noreply.github.com>
2023-04-18 10:36:26 +0700

    Update TASASD_With_Sorting.c

--- TASASD_With_Sorting.c
+++ TASASD_With_Sorting.c
@@ -9,25 +9,52 @@ void gotoxy(int x, int y){
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-int kebutuhan_gizi = 2000;
-int gizi[]={680, 628, 740, 600, 480, 320, 290};
+int kebutuhan_kalori = 2000;
+int kalori[]={680, 628, 740, 600, 480, 320, 290};
 int uang;
+int ulang = 0;
     
 struct perbandingan{
-	int gizi;
+	int kalori;
 	int harga;
 	char makanan[40];
 	int harga_asli;
 };
 
+void Menu_Animation(){
+	int end, count = 0, i, j, x=1, y=1;
+	
+	while (count!=162){
+		gotoxy(x, y);
+		printf("%c", 475);
+		
+		count++;
+		
+		if(count<65){
+			x++;
+		}else if(count>=65 && count < 82){
+			y++;
+		}else if(count>= 82 && count < 146){
+			x--;
+		}else if(count>= 146 && count < 162){
+			y--;
+		}
+		
+		if(ulang == 0){
+			for (i=1;i<=2300;i++)
+				for (j=1;j<=2300;j++);
+		}
+	}
+}
+
 int sort(const void *v1, const void *v2){
 	struct perbandingan *m1 = (struct perbandingan *)v1;
 	struct perbandingan *m2 = (struct perbandingan *)v2;
 	
-	if (m2->gizi == m1->gizi){
+	if (m2->kalori == m1->kalori){
 		return m1->harga - m2->harga;
 	}
-	else if (m2->gizi != m1->gizi){
+	else if (m2->kalori != m1->kalori){
 		return ( *(int*)m2 - *(int*)m1 );
 	}
 }
@@ -43,77 +70,78 @@ int main(){
     
     system("cls");
     
-	gotoxy(1,1);
-	printf("Daftar yang ingin dibeli :");
-	gotoxy(1,3);
+    Menu_Animation();
+    
+	gotoxy(20,2);
+	printf("Daftar yang ingin dibeli");
+	gotoxy(3,4);
 	printf("1. %s\t= Rp. %i,", makanan[0], harga[0]);
-	gotoxy(50,3);
-	printf("Gizi \t= %i", gizi[0]);
+	gotoxy(50,4);
+	printf("kalori = %i", kalori[0]);
 	
-	gotoxy(1,5);
+	gotoxy(3,6);
 	printf("2. %s \t\t= Rp. %i,", makanan[1], harga[1]);
-	gotoxy(50,5);
-	printf("Gizi \t= %i", gizi[1]);
+	gotoxy(50,6);
+	printf("kalori = %i", kalori[1]);
 	
-	gotoxy(1,7);
-	printf("3. %s \t= Rp. %i,", makanan[2], harga[2]);
-	gotoxy(50,7);
-	printf("Gizi \t= %i", gizi[2]);
+	gotoxy(3,8);
+	printf("3. %s = Rp. %i,", makanan[2], harga[2]);
+	gotoxy(50,8);
+	printf("kalori = %i", kalori[2]);
 	
-	gotoxy(1,9);
+	gotoxy(3,10);
 	printf("4. %s \t\t= Rp. %i,", makanan[3], harga[3]);
-    gotoxy(50,9);
-	printf("Gizi \t= %i", gizi[3]);
+    gotoxy(50,10);
+	printf("kalori = %i", kalori[3]);
 	
-	gotoxy(1,11);
-	printf("5. %s \t\t= Rp. %i,", makanan[4], harga[4]);
-	gotoxy(50,11);
-	printf("Gizi \t= %i", gizi[4]);
+	gotoxy(3,12);
+	printf("5. %s \t= Rp. %i,", makanan[4], harga[4]);
+	gotoxy(50,12);
+	printf("kalori = %i", kalori[4]);
 	
-	gotoxy(1,13);
+	gotoxy(3,14);
 	printf("6. %s \t\t\t= Rp. %i,", makanan[5], harga[5]);
-	gotoxy(50,13);
-	printf("Gizi \t= %i", gizi[5]);
+	gotoxy(50,14);
+	printf("kalori = %i", kalori[5]);
 	
-	gotoxy(1,15);
+	gotoxy(3,16);
 	printf("7. %s	\t= Rp. %i,", makanan[6], harga[6]);
-	gotoxy(50,15);
-	printf("Gizi \t= %i", gizi[6]);
+	gotoxy(50,16);
+	printf("kalori = %i", kalori[6]);
 	
-    gotoxy(75,3);
+    gotoxy(78,3);
 	printf("Budget/uang kamu : ");
     scanf("%i", &uang);
     
-    gotoxy(1,16);
     sisa_uang = uang;
     
     for(i=0;i<7;i++){
-		banding[i].gizi = kebutuhan_gizi/gizi[i];
+		banding[i].kalori = kebutuhan_kalori/kalori[i];
 		
     	check:
-		if((banding[i].harga_asli * banding[i].gizi) > uang){
-			banding[i].gizi -= 1;
+		if((banding[i].harga_asli * banding[i].kalori) > uang){
+			banding[i].kalori -= 1;
 			goto check;
-		}else if((banding[i].harga_asli * banding[i].gizi) <= uang){
-			if((banding[i].harga_asli * (banding[i].gizi + 1)) <= uang && (banding[i].gizi + 1) * gizi[i] <= kebutuhan_gizi + 650){
-				banding[i].gizi += 1;
-	    		banding[i].harga = banding[i].harga_asli * banding[i].gizi;
+		}else if((banding[i].harga_asli * banding[i].kalori) <= uang){
+			if((banding[i].harga_asli * (banding[i].kalori + 1)) <= uang && (banding[i].kalori + 1) * kalori[i] <= kebutuhan_kalori + 650){
+				banding[i].kalori += 1;
+	    		banding[i].harga = banding[i].harga_asli * banding[i].kalori;
 				goto check;
 			}
-	    	banding[i].harga = banding[i].harga_asli * banding[i].gizi;
+	    	banding[i].harga = banding[i].harga_asli * banding[i].kalori;
 		}
 		
 //    					cara kerja
 //    	printf("\n%i", banding[i].harga_asli);
-//		printf(" %i", banding[i].gizi);
-//		printf(" Gizi : %i", banding[i].gizi*gizi[i]);
+//		printf(" %i", banding[i].kalori);
+//		printf(" Gizi : %i", banding[i].kalori*kalori[i]);
 //    	printf(" %s", banding[i].makanan);
 //    	printf(" total : %i", banding[i].harga);
 	}
 	
 	
 	for(i=0;i<7;i++){
-		banding[i].gizi = banding[i].gizi*gizi[i];
+		banding[i].kalori = banding[i].kalori*kalori[i];
 	}
 	
 	qsort(banding, 7, sizeof(struct perbandingan), sort);
@@ -122,30 +150,32 @@ int main(){
 //	printf("\n");
 //	for(i=0;i<7;i++){
 //    	printf("\n%i", banding[i].harga_asli);
-//		printf(" %i", banding[i].gizi);
+//		printf(" %i", banding[i].kalori);
 //    	printf(" %s", banding[i].makanan);
 //    	printf(" total : %i", banding[i].harga);
 //	}
 	
 	if(uang < 2600){
-		gotoxy(65,7);
+		gotoxy(68,7);
 		printf("Kamu terlalu miskin untuk membeli makanan...");
 	}else{
-		gotoxy(65,7);
+		gotoxy(68,7);
 		printf("Makanan yang paling optimal \t: %s", banding[0].makanan);
-		gotoxy(65,9);
+		gotoxy(68,9);
 	    printf("Hanya dengan memakan makanan tersebut sebanyak %i kali", banding[0].harga/banding[0].harga_asli);
-		gotoxy(65,10);
-		printf("Kamu mendapatkan total gizi \t: %i", banding[0].gizi, kebutuhan_gizi, kebutuhan_gizi+650);
-		gotoxy(65,11);
-		printf("Sedangkan remaja membutuhkan sebanyak %i - %i per hari", kebutuhan_gizi, kebutuhan_gizi+650);
-		gotoxy(65,12);
+		gotoxy(68,10);
+		printf("Kamu mendapatkan total gizi \t: %i", banding[0].kalori, kebutuhan_kalori, kebutuhan_kalori+650);
+		gotoxy(68,11);
+		printf("Sedangkan remaja membutuhkan sebanyak %i - %i per hari", kebutuhan_kalori, kebutuhan_kalori+650);
+		gotoxy(68,12);
 		printf("Uang yang dihabiskan \t\t: %i", banding[0].harga);
-		gotoxy(65,13);
+		gotoxy(68,13);
 		printf("Sisa uang kamu \t\t: %d\n\n\n", uang - banding[0].harga);
 	}
 	
-	gotoxy(65,15);
+	ulang++;
+	
+	gotoxy(67,15);
 	
 	system("pause");
 	main();
